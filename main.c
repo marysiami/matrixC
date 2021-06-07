5d5daab12833ead5087ed7f41318d501bccbbbe9
Marysia <Marysia@DESKTOP-0ETE4IP>
2021-06-07 11:34:19 +0200

    sekwencyjny algorytm

--- main.c
+++ main.c
@@ -4,35 +4,55 @@
 #include <string.h>
 #include <time.h>
 
-int N1;
-int N2;
+int N;
 
-void multiply_two_matrix() {
+double** multiply_two_matrix() {
 
-    int** matrix1 = malloc(sizeof(int*) * N1);
-    int** matrix2 = malloc(sizeof(int*) * N2);
+    double** matrix1 = malloc(sizeof(int*) * N);
+    double** matrix2 = malloc(sizeof(int*) * N);
+    double** result_matrix = malloc(sizeof(int*) * N);
 
-    int i, j;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            if (i == j || i == size - j - 1) {
-                board[i][j] = 1;
-            }
+    srand(time(NULL));   
+    double r1,r2; 
+    
+    //wypełnienie tablic liczbami losowymi a  tablicy wynikowej zerami
+    int i, j, k;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+
+                r1 = (double)rand();
+                r2 = (double)rand();
+
+                matrix1[i][j] = r1;
+                matrix1[i][j] = r2;
+
+                result_matrix[i][j] = 0;
+        }
+    }  
+
+    //mnożenie macierzy
+    for(i = 0; i <N; ++i){
+        for(j = 0; j < N; ++j){
+           for(k = 0; k < N; ++k)
+            {
+                result_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+            } 
         }
     }
+        
+    return result_matrix;          
 }
 
 int main(int argc, char** argv) {
     srand(time(NULL));
 
-    // Rozmiar bloku K (a także N) powinien być parametrem przekazywanym programowi z wiersza polecenia. 
- 
-    int N1 = atoi(argv[1]);
-    int N2 = atoi(argv[2]);
+    // Rozmiar bloku K (a także N) powinien być parametrem przekazywanym programowi z wiersza polecenia.  
+    int N = atoi(argv[1]);  
+    int K = atoi(argv[2]);
 
-    multiply_two_matrix();
+    double** result_matrix = multiply_two_matrix();
 
-    int K_size = atoi(argv[3]);
+  
 
     // clock_t start, end;
     // double total_time;
